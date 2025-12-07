8b069809739ded02ecedaaba58893defe6c1ba8f
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2025-12-07 01:39:39 +0000

    feat: add `product table`

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <div className="flex h-full gap-8">
+        <div className="flex h-full">
           <Sidebar />
           {children}
         </div>
---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,5 @@
+const ProductsPage = async () => {
+  return <h1>Products Page</h1>;
+};
+
+export default ProductsPage;
